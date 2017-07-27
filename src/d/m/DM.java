8e5d3d01d13b8f0022272ba06cb956ebf3d2361b
Student <Student@192.168.0.39>
2017-07-27 18:41:58 +0200

    kontrola delitelnosti muz/zena

--- src/d/m/DM.java
+++ src/d/m/DM.java
@@ -17,5 +17,23 @@ public class DM {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public static void kontroladelitelnosti11() {
+        byte[] aPole = new byte[10];
+
+        if ((aPole[0] + aPole[2] + aPole[4] + aPole[6] + aPole[8]) == (aPole[1] + aPole[3] + aPole[5] + aPole[7] + aPole[9])) {
+            System.out.println("je to rodne cislo");
+        }
+    }
+
+    public static void muzZena() {
+        byte[] aPole = new byte[10];
+
+        if ((aPole[3] == 5) || (aPole[3] == 4)) {
+            System.out.println("zena");
+        }
+        else if ((aPole[3] == 0) || (aPole[3] == 1)) {
+            System.out.println("muz");
+        }
+        else {System.out.println("neplatne cislo");}
+    }
 }
