dc01b8588c250f40307ed5a797db9b786e9855d2
Student <Student@192.168.0.214>
2017-07-27 18:51:31 +0200

    přidávám funkce: kontrola délky 10 převedení na pole

--- src/d/m/DM.java
+++ src/d/m/DM.java
@@ -11,6 +11,33 @@ package d.m;
  */
 public class DM {
 
+    //funkce na kontrolu délky 10
+        public static boolean zkontrolujDelku (String a) {
+            boolean pom = true;
+            if (a.length() != 10) {
+                System.out.println("Chyba: nezadal jsi 10 číslic");
+                pom = false;
+            }
+            return pom;
+        }
+
+    //převedení na pole longů s kontrolou, jsou-li to čísla    
+        public static byte[] prevedNaPole (String a) {
+            char [] poleCharu = a.toCharArray();
+            byte [] aPole = new byte [11];
+            aPole[10] = 1;  //délka pole je 10 + 11.člen nese info o kontrole
+            for (int i=0; i<10; i++) {
+                if (poleCharu[i]<'0' || poleCharu[i]>'9') {
+                    System.out.println("Chyba: nezadal jsi 10 číslic");
+                    aPole[10] = 0;
+                } else {
+                    aPole[i] = (byte) poleCharu[i];
+                }
+            }
+            return aPole;
+        }
+    
+    
     /**
      * @param args the command line arguments
      */
